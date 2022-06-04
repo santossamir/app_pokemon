11e80789fbdedd55034ab335b1303846ff294d27
Samir Santos <santossamir@hotmail.com>
2022-06-04 00:08:49 -0300

    Desenvolvendo a page Pokemons, iniciando com as divs.

--- src/pages/pokemons/Pokemons.jsx
+++ src/pages/pokemons/Pokemons.jsx
@@ -1,11 +1,52 @@
 import React from "react";
+import "../../css/pokemons.css";
 import Navbar from "../../components/Navbar";
+import search from "../../img/iconSearch.svg";
 
 export default function Pokemons(){
     return(
         <>
-        <Navbar/>
-            I'm Pokemons!
+            <Navbar/>
+            <div className="containerPokemons">
+                <div className="pokemonsTittle">
+                    <small> Mais de 250 Pokemons para você escolher o seu favorito</small>
+                </div>
+                <div className="pokemonsSearch">
+                    
+                    <input type="text" placeholder="Pesquisar pokemon"/>
+                    
+                    <button type="submit">
+                        <img src={search}/>
+                    </button>
+
+                </div>
+                <div className="pokemonsSelects">
+                    <select>
+                        <option>Tipo</option>
+                        <option>Fogo</option>
+                        <option>Planta</option>
+                        <option>Elétrico</option>
+                        <option>Água</option>
+                        <option>Normal</option>
+                    </select>
+
+                    <select>
+                        <option>Ataque</option>
+                    </select>
+
+                    <select>
+                        <option>Defesa</option>
+                    </select>
+                </div>
+                <div className="pokemonsCards">
+                    <div className="cards">
+
+                    </div>
+                    <div className="cards2">
+
+                    </div>
+                </div>
+            </div>
         </>
    
     )
