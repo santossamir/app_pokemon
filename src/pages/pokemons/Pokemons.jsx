5f05961ac1627b345a7f63b1cf88d726cb076cae
Samir Santos <santossamir@hotmail.com>
2022-06-04 11:18:04 -0300

    Desenvolvendo e estilizando os cards.

--- src/pages/pokemons/Pokemons.jsx
+++ src/pages/pokemons/Pokemons.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "../../css/pokemons.css";
 import Navbar from "../../components/Navbar";
 import search from "../../img/iconSearch.svg";
+import {api} from "../../service/api.js";
+import img from "../../img/bulbasaurPoke.svg";
 
 export default function Pokemons(){
+
+    //const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=18');
+    const [pokemons, setPokemons] = useState([]);
+
+async function showPokemons(){
+    await api.get('pokemon/').then(response => {
+        setPokemons(response.data);
+        console.log(response.data);
+    })
+} 
+
+useEffect(()=>{
+    showPokemons();
+}, [])
+
+function getAllPokemons(){
+    alert('ok');
+}
     return(
         <>
             <Navbar/>
@@ -39,11 +59,86 @@ export default function Pokemons(){
                     </select>
                 </div>
                 <div className="pokemonsCards">
-                    <div className="cards">
+                    <div className="boxCards">
+                    
+                        <div className="cards">
+                            <div className="cardNumber">
+                                <small>#001</small>
+                            </div>
+                            <div className="cardName">
+                                <small>Bulbasaur</small>
+                            </div>
+                            <div className="cardCategoriesImage">
+                                < div className="cardCategories">
+                                    <div className="type">
+                                        <small>Planta</small>
+                                    </div>
+                                    <div className="typeTwo">
+                                        <small>Venenoso</small>
+                                    </div>
+                                </div>
+                                <div className="cardImage">
+                                    <img src={img}/>
+                                </div>
+                            </div>
+                              
+                        </div>
+                        <div className="cards2">
 
+                        </div>
+                        <div className="cards">
+
+                        </div>
+                        <div className="cards2">
+
+                        </div>
+                        <div className="cards">
+
+                        </div>
+                        <div className="cards2">
+
+                        </div>
+                        <div className="cards">
+
+                        </div>
+                        <div className="cards2">
+
+                        </div>
+                        <div className="cards">
+
+                        </div>
+                        <div className="cards2">
+
+                        </div>
+                        <div className="cards">
+
+                        </div>
+                        <div className="cards2">
+
+                        </div>
+                        <div className="cards">
+
+                        </div>
+                        <div className="cards2">
+
+                        </div>
+                        <div className="cards">
+
+                        </div>
+                        <div className="cards2">
+
+                        </div>
+                        <div className="cards">
+
+                        </div>
+                        <div className="cards2">
+
+                        </div>
                     </div>
-                    <div className="cards2">
-
+                    <div className="cardsButtonMore">
+                        <button onClick={() => getAllPokemons()}>
+                            More
+                        </button>
                     </div>
                 </div>
             </div>
